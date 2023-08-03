c72b127e86aa3b86a8193443fa526478cb444850
godot23 <jasonanavon@gmail.com>
2023-08-03 14:54:25 -0400

    I added a bunch of things here. I created a for loop to add appropriate time classes to the sections, and created a loop that retrieves data from localStorage to autofill the sections with previous info.

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,69 @@
+let date = document.querySelector("#currentDay")
+let container = document.querySelector("body")
+let times = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+
+  
+
+
+
+  container.addEventListener("click", function(event){
+    let element = event.target;
+    if(element.matches("button")){  
+      let section = element.getAttribute("data-time");
+      let memoLocation = element.previousElementSibling;
+      let memo = memoLocation.value;
+      localStorage.setItem(section, memo);
+    }
+    else if (element.matches("i")){
+      console.log(element);
+      let section = element.parentElement.getAttribute("data-time");
+      let memoLocation = element.parentElement.previousElementSibling;
+      let memo = memoLocation.value;
+      console.log(memoLocation);
+      localStorage.setItem(section, memo);
+    
+    }
+  })
+  
+  for(let i = 0; i < times.length; i++){
+    console.log(dayjs().hour)
+    if (dayjs().hour() < times[i]){
+      let part1 = "hour-";
+      let part2 = JSON.stringify(times[i]);
+      let existingId = part1.concat(part2);
+      document.getElementById(existingId).classList.add("future");
+    }
+  
+    else if(dayjs().hour() === times[i]){
+      let part1 = "hour-";
+      let part2 = JSON.stringify(times[i]);
+      let existingId = part1.concat(part2)
+      document.getElementById(existingId).classList.add("present")
+    }
+  
+    else{
+      let part1 = "hour-";
+      let part2 = JSON.stringify(times[i]);
+      let existingId = part1.concat(part2)
+      document.getElementById(existingId).classList.add("past")
+    }
+  }
+
+  for(let i = 0; i < times.length; i++){
+    let timeString = JSON.stringify(times[i]);
+    let area = document.querySelector(`#hour-${timeString} textArea`);
+    console.log(area);
+    let contentOfText = localStorage.getItem(timeString);
+    area.value = contentOfText;
+
+  }
   // TODO: Add code to display the current date in the header of the page.
+  date.textContent = dayjs().format("dddd, MMMM DD");
 });
